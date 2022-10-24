59a40fd09eae10f046db064e3c8eb9228dea1fee
Ihor <dovhanych.ihor@student.uzhnu.edu.ua>
2022-10-24 08:28:58 +0300

    finish lab

--- Lab3/index.php
+++ Lab3/index.php
@@ -4,6 +4,21 @@ use Model\Factory;
 use Model\Factory\Collection;
 use Model\Factory\Repository;
 
-$factory = new Factory(1, 'Dik', 50, 'bakery', 'Uzhorod');
+function myAutoloader($class_name)
+{
+    if (!class_exists($class_name)) {
+        include $class_name . '.php';
+    }
+}
 
-var_dump($factory->getId());
+spl_autoload_register('myAutoloader');
+
+
+$factory1 = new Factory(1, 'Dik', 50, 'bakery', 'Uzhorod');
+$factory2 = new Factory(2, 'PoloninskiyHlib', 50, 'bakery', 'Uzhorod');
+
+$factoryCollection = new Collection([$factory1, $factory2]);
+$saveFactoryCollection = new Repository();
+var_dump($saveFactoryCollection->loadDataFromFile('ok'));
+//$saveFactoryCollection->createNewFile('ok');
+//$saveFactoryCollection->storeDataToFile($factoryCollection,'ok');
